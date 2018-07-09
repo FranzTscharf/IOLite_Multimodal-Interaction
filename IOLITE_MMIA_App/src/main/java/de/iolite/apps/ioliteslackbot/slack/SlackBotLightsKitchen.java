bcb0466bde52ecb5e7bdb62970f60dd11b15237e
Marc Ottenbacher <ottenbacher.marc@gmail.com>
2018-07-09 18:38:20 +0200

    added class for lights in kitchen
    also had to change some of the HttpStatus.
    HTTPStatus.UNAUTHORIZED... because it was marked as error in my IDE

---
+++ IOLITE_MMIA_App/src/main/java/de/iolite/apps/ioliteslackbot/slack/SlackBotLightsKitchen.java
@@ -0,0 +1,15 @@
+package de.iolite.apps.ioliteslackbot.slack;
+
+import de.iolite.app.api.device.DeviceAPIException;
+import de.iolite.app.api.device.access.Device;
+import de.iolite.app.api.device.access.DeviceBooleanProperty;
+import de.iolite.apps.ioliteslackbot.IoLiteSlackBotApp;
+import de.iolite.drivers.basic.DriverConstants;
+import org.riversun.slacklet.SlackletRequest;
+import org.riversun.slacklet.SlackletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.Nonnull;
+
+public class SlackBotLightsKitchen {
+}
