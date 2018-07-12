e289f68dba1ddf7160a7c98e18fbda3d470d484c
Marc Ottenbacher <ottenbacher.marc@gmail.com>
2018-07-12 11:28:43 +0200

    untested Kitchen Lights feature
    couldnt test it with my laptop yet. will try later. Added Device Mapping Controller which maps .environment.devices with .device.access.devices via getIdentifier

--- IOLITE_MMIA_App/src/main/java/de/iolite/apps/ioliteslackbot/slack/SlackBotLightsKitchen.java
+++ IOLITE_MMIA_App/src/main/java/de/iolite/apps/ioliteslackbot/slack/SlackBotLightsKitchen.java
@@ -11,8 +11,9 @@ package de.iolite.apps.ioliteslackbot.slack;
         import org.riversun.slacklet.SlackletResponse;
         import org.slf4j.Logger;
         import org.slf4j.LoggerFactory;
+        import static de.iolite.apps.ioliteslackbot.messagecontroller.DeviceMappingController.*;
 
-        import java.awt.*;
+
         import java.util.ArrayList;
         import java.util.List;
 
@@ -20,10 +21,15 @@ package de.iolite.apps.ioliteslackbot.slack;
 
         import javax.annotation.Nonnull;
 
-        import static de.iolite.apps.ioliteslackbot.slack.SlackDirectMessageController.getDecisionTree;
-
+/**
+ * Kitchen lights will be turned on here
+ *
+ * @author Marc Ottenbacher
+ * @since 05.07.2018
+ */
 public class SlackBotLightsKitchen extends Slacklet {
     private IoLiteSlackBotApp app;
+
 
     public void setApp(IoLiteSlackBotApp setApp){
         this.app = setApp;
@@ -31,63 +37,58 @@ public class SlackBotLightsKitchen extends Slacklet {
     private final ArrayList<String> wordArrayList = new ArrayList<String>();
 
 
+
     @Nonnull
     private static final Logger LOGGER = LoggerFactory.getLogger(IoLiteSlackBotApp.class);
-
+/*
     public ArrayList getCommandArray(String initCommand) {
         for (String fullCommand : initCommand.split(" ")) {
             wordArrayList.add(fullCommand);
         }
         return wordArrayList;
     }
+*/
 
 
     public void setlightsKitchen(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app){
-        //get user message
         String slackUsrMsg = req.getContent();
-        //put user message into a string array (data preperation)
-        ArrayList <String> usrMsg = getCommandArray(slackUsrMsg);
-        // check whether kitchen was found in user message
-        Location currentLoc = checkRoom(usrMsg);
-        //if found turn lights on else return
-        if(currentLoc!=null){
-            for (final Device device : app.getDeviceAPI().getDevices()) {
-                // togle the on trigger!
+        //ArrayList <String> usrMsg = getCommandArray(slackUsrMsg);
+        List<Device> currentLocationDevices = getCurrentLocationDevices();
+            for (final Device device : currentLocationDevices) {
                 final DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROPERTY_on_ID);
                 final Boolean onValue;
                 if (onProperty != null && (onValue = onProperty.getValue()) != null) {
                     //get only lamps
+                    if (device.getModelName().toLowerCase().contains("lamp")) {
                     try {
                         //toge on propertie
                         onProperty.requestValueUpdate(!onValue);
                         //give terminal output
                         LOGGER.warn("toggling device '{}'", device.getIdentifier());
                         //return the name of the device
-                        resp.reply("I switched"+device.getIdentifier() + "on");
+                        resp.reply("Swichted java -jar iolite.jarjava -jar iolite.jaron following devices "+device.getIdentifier() + "in the kitchen");
                     }
                     catch (final DeviceAPIException e) {
-                        LOGGER.error("Failed to control device", e);
+                        LOGGER.error("Could not switch on the lights in the kitchen", e);
+
                     }
-                    //}
+
                 }
             }
         }
 
     }
     
-    public Location checkRoom(ArrayList<String> usrMsg){
-        List<Location> rooms = app.getEnvironmentAPI().getLocations();
+   public List<Device> getCurrentLocationDevices() {
+       List<Location> rooms = app.getEnvironmentAPI().getLocations();
+       List<Device> locationDeviceList = new ArrayList<>();
+       String deviceID;
+       for (Location l: rooms) {
+           if(l.getName().toLowerCase().contains("kitchen")){
+               locationDeviceList = mapDevices(l.getDevices(), app);
+           }
+       }
 
-        for (String temp : usrMsg){
-            for(Location l : rooms) {
-                if(temp == l.getName()) {
-                    if (temp.toLowerCase() == "kitchen"){
-                        return l;
-                    }
-                    else {continue;}
-                }
-            }
-        }
-        return null;
+   return locationDeviceList;
     }
 }
--- IOLITE_MMIA_App/src/main/java/de/iolite/apps/ioliteslackbot/slack/SlackDirectMessageController.java
+++ IOLITE_MMIA_App/src/main/java/de/iolite/apps/ioliteslackbot/slack/SlackDirectMessageController.java
@@ -20,131 +20,133 @@ import de.iolite.drivers.basic.DriverConstants;
 
 public class SlackDirectMessageController {
 
-	@Nonnull
-	private static final Logger LOGGER = LoggerFactory.getLogger(IoLiteSlackBotApp.class);
+    @Nonnull
+    private static final Logger LOGGER = LoggerFactory.getLogger(IoLiteSlackBotApp.class);
 
-	public static void getDecisionTree(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
-		switch (req.getContent()) {
-		case "turn all lights on":
-			LOGGER.warn(req.getContent());
-			lightAll(req, resp, app);
-			break;
-		case "switch all lights on":
-			break;
-		case "turn all lights off":
-			System.out.print("");
-			break;
-		case "switch all lights off":
-			break;
-		case "is the heater off":
-			isTheHeatherOff(req, resp, app);
-			break;
-		case "getAllDeviceNames":
-			getAllDeviceNames(req, resp, app);
-			break;
-		default:
-			// ask DialogFlow of a response if the other case don't fit;
-			LOGGER.warn("DialogFlow Request");
-			getDialogFlow(req, resp, app);
-		}
-	}
-	
-	
+    public static void getDecisionTree(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
+        switch (req.getContent()) {
+            case "turn all lights on":
+                LOGGER.warn(req.getContent());
+                lightAll(req, resp, app);
+                break;
+            case "switch all lights on":
+                break;
+            case "turn all lights off":
+                System.out.print("");
+                break;
+            case "switch all lights off":
+                break;
+            case "is the heater off":
+                isTheHeatherOff(req, resp, app);
+                break;
+            case "getAllDeviceNames":
+                getAllDeviceNames(req, resp, app);
+                break;
+            case "turn on the lights in the kitchen":
+                getAllDeviceNames(req, resp, app);
+                break;
+            default:
+                // ask DialogFlow of a response if the other case don't fit;
+                LOGGER.warn("DialogFlow Request");
+                getDialogFlow(req, resp, app);
+        }
+    }
 
-	public static void lightAll(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
-		resp.reply("Sure think! I switched on the following devices:");
-		// iterate devices
-		for (final Device device : app.getDeviceAPI().getDevices()) {
-			// togle the on trigger!
-			final DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROPERTY_on_ID);
-			final Boolean onValue;
-			if (onProperty != null && (onValue = onProperty.getValue()) != null) {
-				// get only lamps
-				if (device.getModelName().contains("Lamp") || device.getModelName().contains("lamp")) {
-					try {
-						// toge on propertie
-						onProperty.requestValueUpdate(!onValue);
-						// give terminal output
-						LOGGER.warn("toggling device '{}'", device.getIdentifier());
-						// return the name of the device
-						resp.reply("I switched" + device.getIdentifier() + "on");
-					} catch (final DeviceAPIException e) {
-						LOGGER.error("Failed to control device", e);
-					}
-				}
-			}
-		}
-	}
 
-	public static void isTheHeatherOff(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
-		// iterate devices
-		ArrayList<Device> devices = new ArrayList<Device>();
-		for (final Device device : app.getDeviceAPI().getDevices()) {
-			// togle the on trigger!
-			if (device.getIdentifier().toLowerCase().contains("")) {
-				devices.add(device);
-			}
-		}
+    public static void lightAll(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
+        resp.reply("Sure think! I switched on the following devices:");
+        // iterate devices
+        for (final Device device : app.getDeviceAPI().getDevices()) {
+            // togle the on trigger!
+            final DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROPERTY_on_ID);
+            final Boolean onValue;
+            if (onProperty != null && (onValue = onProperty.getValue()) != null) {
+                // get only lamps
+                if (device.getModelName().contains("Lamp") || device.getModelName().contains("lamp")) {
+                    try {
+                        // toge on propertie
+                        onProperty.requestValueUpdate(!onValue);
+                        // give terminal output
+                        LOGGER.warn("toggling device '{}'", device.getIdentifier());
+                        // return the name of the device
+                        resp.reply("I switched" + device.getIdentifier() + "on");
+                    } catch (final DeviceAPIException e) {
+                        LOGGER.error("Failed to control device", e);
+                    }
+                }
+            }
+        }
+    }
 
-		StringBuilder sb = new StringBuilder();
+    public static void isTheHeatherOff(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
+        // iterate devices
+        ArrayList<Device> devices = new ArrayList<Device>();
+        for (final Device device : app.getDeviceAPI().getDevices()) {
+            // togle the on trigger!
+            if (device.getIdentifier().toLowerCase().contains("")) {
+                devices.add(device);
+            }
+        }
 
-		if (devices.size() == 0) {
-			sb.append("I cound not find the heater");
-		}
+        StringBuilder sb = new StringBuilder();
 
-		for (Device dev : devices) {
-			sb.append("Heater " + dev.getName() + " has the temperature: "
-					+ dev.getStringProperty(
-							DriverConstants.PROFILE_PROPERTY_TemperatureSensor_currentEnvironmentTemperature_ID)
-					+ "\n");
-		}
-		try {
-			// toge on propertie
+        if (devices.size() == 0) {
+            sb.append("I cound not find the heater");
+        }
 
-			// give terminal output
-			LOGGER.warn(sb.toString());
-			// return the name of the device
-			resp.reply(sb.toString());
-		} catch (final Exception e) {
-			LOGGER.error("Failed to control device", e);
-		}
+        for (Device dev : devices) {
+            sb.append("Heater " + dev.getName() + " has the temperature: "
+                    + dev.getStringProperty(
+                    DriverConstants.PROFILE_PROPERTY_TemperatureSensor_currentEnvironmentTemperature_ID)
+                    + "\n");
+        }
+        try {
+            // toge on propertie
 
-	}
+            // give terminal output
+            LOGGER.warn(sb.toString());
+            // return the name of the device
+            resp.reply(sb.toString());
+        } catch (final Exception e) {
+            LOGGER.error("Failed to control device", e);
+        }
 
-	public static void getAllDeviceNames(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
-		// iterate devices
-		StringBuilder sb = new StringBuilder();
-		sb.append("I found the following devices:" + "\n");
-		for (final Device device : app.getDeviceAPI().getDevices()) {
-			if (device.getName() != null)
-				sb.append(device.getName() + " : " + device.getIdentifier() + " : " + device.getProfileIdentifier()
-						+ " : " + device.getStringProperty(DriverConstants.PROFILE_PROPERTY_Device_deviceStatus_ID) + "\n");
-		}
-		try {
-			LOGGER.warn(sb.toString());
-			resp.reply(sb.toString());
-		} catch (final Exception e) {
-			LOGGER.error("Failed to control device", e);
-		}
+    }
 
-	}
-	
-	public static void getAllRoomNames(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
-		// iterate devices
-		StringBuilder sb = new StringBuilder();
-		sb.append("I found the following Rooms:" + "\n");
-		for (final Location location : app.getEnvironmentAPI().getLocations()) {
-			if (location.getName() != null)
-				sb.append(location.getName()+"\n");
-		}
-		try {
-			LOGGER.warn(sb.toString());
-			resp.reply(sb.toString());
-		} catch (final Exception e) {
-			LOGGER.error("Failed to control device", e);
-		}
+    public static void getAllDeviceNames(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
+        // iterate devices
+        StringBuilder sb = new StringBuilder();
+        sb.append("I found the following devices:" + "\n");
+        for (final Device device : app.getDeviceAPI().getDevices()) {
+            if (device.getName() != null)
+                sb.append(device.getName() + " : " + device.getIdentifier() + " : " + device.getProfileIdentifier()
+                        + " : " + device.getStringProperty(DriverConstants.PROFILE_PROPERTY_Device_deviceStatus_ID) + "\n");
+        }
+        try {
+            LOGGER.warn(sb.toString());
+            resp.reply(sb.toString());
+        } catch (final Exception e) {
+            LOGGER.error("Failed to control device", e);
+        }
 
-	}
-	
-	
+    }
+
+    public static void getAllRoomNames(SlackletRequest req, SlackletResponse resp, IoLiteSlackBotApp app) {
+        // iterate devices
+        StringBuilder sb = new StringBuilder();
+        sb.append("I found the following Rooms:" + "\n");
+        for (final Location location : app.getEnvironmentAPI().getLocations()) {
+            if (location.getName() != null)
+                sb.append(location.getName() + "\n");
+        }
+        try {
+            LOGGER.warn(sb.toString());
+            resp.reply(sb.toString());
+        } catch (final Exception e) {
+            LOGGER.error("Failed to control device", e);
+        }
+
+    }
+
+
 }
